20d8c21fa2e8c53e056c4317bcb503281f9bfb78
MartinDampier <Marty.dampier@gmail.com>
2021-10-21 21:54:25 -0500

    Added Assets folder. Added a Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,15 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget
+{
   const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
 
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context)
+  {
     return MaterialApp(
       title: 'Sound Trek',
       theme: ThemeData(
@@ -27,7 +29,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primaryColor: Color.fromARGB(255, 98, 98, 98),
+        primaryColor: const Color.fromARGB(255, 98, 98, 98),
         primarySwatch: Colors.blue,
         canvasColor: Colors.grey,
       ),
@@ -36,7 +38,8 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatefulWidget
+{
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
@@ -54,28 +57,18 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  Completer<GoogleMapController> _controller = Completer();
-  static final CameraPosition _kGooglePlex = CameraPosition(
+class _MyHomePageState extends State<MyHomePage>
+{
+  final Completer<GoogleMapController> _controller = Completer();
+  static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
   );
-  static final CameraPosition _kLake = CameraPosition(
+  static const CameraPosition _kLake = CameraPosition(
       bearing: 192.8334901395799,
       target: LatLng(37.43296265331129, -122.08832357078792),
       tilt: 59.440717697143555,
       zoom: 19.151926040649414);
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -85,11 +78,23 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
+    return Scaffold
+      (
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(255,149, 215, 201),
         centerTitle: true,
-        title: Text(widget.title, style: TextStyle( color: Color.fromARGB(255, 98, 98, 98),)),
+        title: Text(widget.title, style: const TextStyle( color: Color.fromARGB(255, 98, 98, 98),)),
+        leading: IconButton(
+          icon: const ImageIcon(
+            AssetImage('assets/logos/SoundTrek_Simplified.png'),
+            size: 300,
+          ),
+          onPressed: () => Scaffold.of(context).openDrawer(),
+        ),
+      ),
+
+      drawer: const Drawer(
+        child: Text('Hello'),
       ),
 
       body: GoogleMap(
@@ -99,14 +104,15 @@ class _MyHomePageState extends State<MyHomePage> {
             _controller.complete(controller);
           },
         ),
-      floatingActionButton: FloatingActionButton.extended(
+
+      floatingActionButton: FloatingActionButton(
         onPressed: _goToTheLake,
-        label: Text('To the lake!',style: TextStyle( color: Color.fromARGB(255, 98, 98, 98),) ),
-        icon: Icon(Icons.directions_boat, color: Color.fromARGB(255, 98, 98, 98)),
+        child: const Icon(Icons.menu, color: Color.fromARGB(255, 98, 98, 98)),
         backgroundColor: const Color.fromARGB(255,149, 215, 201),
       ),
     );
   }
+
   Future<void> _goToTheLake() async {
     final GoogleMapController controller = await _controller.future;
     controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
