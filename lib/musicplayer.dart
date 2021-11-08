62df8a04f74f602006f6218da81ec9e7bf939302
Kalob Morel <kamorel255@gmail.com>
2021-11-07 18:57:07 -0600

    Got audio to at least play. I don't like this implementation. Will be wroking on implementing correctly.

--- lib/musicplayer.dart
+++ lib/musicplayer.dart
@@ -4,9 +4,8 @@ import 'package:SoundTrek/utilities/appcolors.dart' as AppColors;
 import 'audiofile.dart';
 
 class MusicPlayer extends StatefulWidget {
-  final booksData;
-  final int index;
-  const MusicPlayer({Key? key, this.booksData, required this.index}) : super(key: key);
+
+  const MusicPlayer({Key? key}) : super(key: key);
 
   @override
   _MusicPlayerState createState() => _MusicPlayerState();
@@ -38,9 +37,9 @@ class _MusicPlayerState extends State<MusicPlayer> {
               color: Colors.white,
               ),
               child: Column(
-                children: [
-                  AudioFile(advancedPlayer: advancedPlayer, audioPath: widget.booksData[widget.index]["audio"])
-                ],
+                // children: [
+                //   AudioFile(advancedPlayer: advancedPlayer, audioPath: widget.booksData[widget.index]["audio"])
+                // ],
               ),
             )),
         Positioned(
@@ -60,10 +59,10 @@ class _MusicPlayerState extends State<MusicPlayer> {
                   decoration: BoxDecoration(
                     shape: BoxShape.circle,
                     border: Border.all(color: Colors.white, width: 5),
-                    image: DecorationImage(
-                      image: AssetImage(widget.booksData[widget.index]["img"]),
-                      fit: BoxFit.cover
-                    )
+                    // image: DecorationImage(
+                    //   image: AssetImage(widget.booksData[widget.index]["img"]),
+                    //   fit: BoxFit.cover
+                    // )
                   ),
                 ),
               ),
