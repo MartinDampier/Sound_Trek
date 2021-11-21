313cce8ce984be2b69427d0a16c15c2ca56568c4
LaSeanS <laseans.28612@gmail.com>
2021-11-21 11:41:27 -0600

    location event builder UI

--- lib/screens/event_builders/location_event_builder.dart
+++ lib/screens/event_builders/location_event_builder.dart
@@ -1,42 +1,204 @@
 import 'package:flutter/material.dart';
-import '../../main.dart';
-import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:location/location.dart';
+import 'package:provider/provider.dart';
+import 'package:sound_trek/models/soundtrack_item.dart';
+import 'package:sound_trek/screens/add_playlists.dart';
+import 'package:sound_trek/models/priority_queue.dart';
+import 'package:sound_trek/models/playlist.dart';
+import 'package:sound_trek/models/events/event.dart';
+import 'package:sound_trek/models/events/location_event.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
-class BuildLocationEvent extends StatelessWidget {
-  Widget build(BuildContext context) => Scaffold(appBar: AppBar(
-    title: const Text("Location Event"),
-    centerTitle: true,
-    backgroundColor: Color.fromARGB(255, 149, 215, 201),
-  ),
-    body: const MyCustomForm(),);
+class BuildLocationEvent extends StatefulWidget {
+  const BuildLocationEvent({Key? key}) : super(key: key);
+
+  @override
+  BuildLocationEventState createState() {
+    return BuildLocationEventState();
+  }
 }
-class MyCustomForm extends StatelessWidget {
-  const MyCustomForm({ Key? key }) : super(key: key);
+
+class BuildLocationEventState extends State<BuildLocationEvent> {
+  GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+
+  late GoogleMapController _controller;
+  Location _location = Location();
+  bool _isServiceEnabled = false;
+  late PermissionStatus _permissionStatus;
+  late LocationData _locationData;
+  bool _isListenLocation = false, _isGetLocation = false;
+  Playlist playlist = Playlist();
+
+  static CameraPosition _initialPosition = CameraPosition(
+    target: LatLng(30.40766724145041, -91.17953531915799),
+    zoom: 14.4746,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    _requestLocationPerms();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Enter a search term',
-            ),
+    final eventsPriorityQueue = Provider.of<PriorityQueue>(context);
+
+    return Scaffold(
+      key: _scaffoldKey,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 149, 215, 201),
+        title: const Text("Choose a Location and Radius"),
+        centerTitle: true,
+        elevation: 4,
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0.0, 0, 0.0, 0.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(
+                height: 420,
+                child: GoogleMap(
+                  mapType: MapType.normal,
+                  initialCameraPosition: _initialPosition,
+                  onMapCreated: _onMapCreated,
+                  myLocationEnabled: true,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        textStyle: const TextStyle(fontSize: 25),
+                        primary: Colors.white,
+                        backgroundColor: const Color.fromARGB(255, 149, 215, 201),
+                      ),
+                      onPressed: () {
+                        setEventRadius();
+                      },
+                      child: Text('500 ft'),
+                    ),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        textStyle: const TextStyle(fontSize: 25),
+                        primary: Colors.white,
+                        backgroundColor: const Color.fromARGB(255, 149, 215, 201),
+                      ),
+                      onPressed: () {
+                        setEventRadius();
+                      },
+                      child: Text('1 mile'),
+                    ),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        textStyle: const TextStyle(fontSize: 25),
+                        primary: Colors.white,
+                        backgroundColor: const Color.fromARGB(255, 149, 215, 201),
+                      ),
+                      onPressed: () {
+                        setEventRadius();
+                      },
+                      child: Text('5 miles'),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 20.0),
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    textStyle: const TextStyle(fontSize: 25),
+                    primary: Colors.white,
+                    backgroundColor: const Color.fromARGB(255, 149, 215, 201),
+                  ),
+                  onPressed: () {
+                    addPlaylists(context);
+                  },
+                  child: Text('Add Playlists'),
+                ),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  textStyle: const TextStyle(fontSize: 25),
+                  primary: Colors.white,
+                  backgroundColor: const Color.fromARGB(255, 149, 215, 201),
+                ),
+                onPressed: () {
+                  createLocationEvent(eventsPriorityQueue);
+                  Navigator.pop(context);
+                },
+                child: Text('Create Event'),
+              ),
+            ],
           ),
         ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextFormField(
-            decoration: const InputDecoration(
-              border: UnderlineInputBorder(),
-              labelText: 'Enter your username',
-            ),
-          ),
-        ),
-      ],
+      ),
     );
   }
+
+  Future<void> addPlaylists(BuildContext context) async {
+    final chosenPlaylist =
+    await Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return AddPlaylists();
+    }));
+
+    setState(() {
+      playlist = chosenPlaylist;
+    });
+  }
+
+  void createLocationEvent(PriorityQueue events) {
+    String eventListName = 'Event ' + (events.possibilities.length + 1).toString();
+    List<Event> eventList = [];
+
+    SoundtrackItem item = SoundtrackItem(playlist, eventList);
+    events.addItem(item);
+  }
+
+  String displayLocation() {
+    return _location.toString();
+  }
+
+  void setEventRadius() {
+
+  }
+
+  void _onMapCreated(GoogleMapController _cntlr) {
+    _controller = _cntlr;
+  }
+
+  Future<void> _requestLocationPerms() async {
+    _isServiceEnabled = await _location.serviceEnabled();
+
+    if (!_isServiceEnabled) {
+      _isServiceEnabled = await _location.requestService();
+      if (_isServiceEnabled) return;
+    }
+
+    _permissionStatus = await _location.requestPermission();
+
+    if (_permissionStatus == PermissionStatus.denied) {
+      _isServiceEnabled = await _location.requestService();
+      if (_isServiceEnabled != PermissionStatus.granted) return;
+    }
+
+    _locationData = await _location.getLocation();
+
+    setState(() {
+      _isGetLocation = true;
+      _initialPosition = CameraPosition(
+        target: LatLng(_locationData.latitude as double, _locationData.longitude as double),
+        zoom: 15,
+      );
+    });
+  }
+
 }
